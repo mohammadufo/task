a743532504720e070f0cd913694801361b8b9e9c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-05-08 23:15:52 +0330

    implement single cart

--- src/Pages/single/SinglePage.tsx
+++ src/Pages/single/SinglePage.tsx
@@ -1,30 +1,64 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { Button, CardActionArea, CardActions, Alert } from '@mui/material'
 
 const SinglePage = () => {
-  let { id } = useParams();
-  let [data, setData] = useState([]);
+  let { id } = useParams()
+  let [data, setData] = useState([])
 
-  let api = `https://rickandmortyapi.com/api/character/${id}`;
-
-  // useEffect(() => {
-  //   (async function () {
-  //     const data = await axios.get(api).then((res) => res.json());
-  //     updateFetchedData(data);
-  //   })();
-  // }, [api]);
+  let api = `https://rickandmortyapi.com/api/character/${id}`
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(api);
-      console.log(response.data);
-      setData(response.data);
-    };
-    fetchData();
-  }, [api]);
+      const response = await axios.get(api)
+      console.log(response.data)
+      setData(response.data)
+    }
+    fetchData()
+  }, [api])
 
-  return <div>SinglePage</div>;
-};
+  return (
+    <div className="wrapper">
+      <Card className="singleCart">
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="250"
+            image={data.image}
+            alt={data.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {data.name}
+            </Typography>
+            <Alert severity="success">{data.status}</Alert>
+            <span>
+              Gender : <strong>{data.gender}</strong>{' '}
+            </span>
+            <span>
+              Location : <strong>{data.location?.name}</strong>{' '}
+            </span>
+            <span>
+              Origin : <strong>{data.origin?.name}</strong>{' '}
+            </span>
+            <span>
+              Species : <strong>{data.species}</strong>{' '}
+            </span>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
+}
 
-export default SinglePage;
+export default SinglePage
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
-import HomePage from "./Pages/home/HomePage.tsx";
-import SinglePage from "./Pages/single/SinglePage.tsx";
+} from 'react-router-dom'
+import HomePage from './Pages/home/HomePage.tsx'
+import SinglePage from './Pages/single/SinglePage.tsx'
+import './main.scss'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomePage />} />
       <Route path="single/:id" element={<SinglePage />} />
-      {/* <Route path="dashboard" element={<Dashboard />} /> */}
-      {/* ... etc. */}
     </Route>
   )
-);
+)
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
